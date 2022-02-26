185633b248096b23dfd367da6407833859d362e7
Sandy7068 <sandeep.singh6065@gmail.com>
2022-02-25 19:12:51 -0500

    typescript practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import Header from "./components/header/header";
+import {CustomMap} from "./classes/CustomMap";
+import {User} from "./classes/User";
+import {Company} from "./classes/Company";
 
 function App() {
+    useEffect(()=>{
+        const customMap = new CustomMap("map");
+        const user = new User();
+        const company = new Company();
+        customMap.addMarker(user);
+        customMap.addMarker(company);
+    },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+        <button id="refresh" onClick={()=>{window.location.reload()}}>Refresh</button>
     </div>
   );
 }
---
+++ src/components/header/header.tsx
@@ -0,0 +1,16 @@
+
+
+const Header = ()=>{
+
+
+    return(
+        <div id="map" style={{height:"600px"}}>
+
+        </div>
+
+    )
+}
+
+export default Header;
+
+
